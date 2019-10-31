8bbdff992fec7f1d1f02e434b862c9a57b4444de
Nicholas Dudfield <ndudfield@gmail.com>
2019-10-31 12:36:27 +0700

    feat: update dependencies (#12)
    * web-monetization: webpack-cli: ^3.3.10
    * web-monetization: @types/jest: ^24.0.21
    * web-monetization: @types/node: ^12.12.3
    * web-monetization: @types/webpack: 4.39.7
    * web-monetization: @typescript-eslint/eslint-plugin: 2.6.x
    * web-monetization: @typescript-eslint/parser: 2.6.x
    * web-monetization: eslint: ^6.6.0
    * web-monetization: eslint-config-prettier: ^6.5.0
    * web-monetization: eslint-plugin-jest: ^23.0.2
    * web-monetization: puppeteer: ^2.0.0
    * coil-monorepo-upkeep: deepmerge: ^4.2.2
    * coil-monorepo-upkeep: npm-check-updates: ^3.1.26
    * web-monetization-polyfill-utils: @types/uuid: ^3.4.6
    * interledger-minute-extension: @types/uuid: ^3.4.6
    * coil-extension: @abraham/reflection: ^0.7.0
    * coil-extension: @material-ui/core: ^4.5.2
    * coil-extension: styled-components: ^4.4.1
    * coil-extension: @types/expect-puppeteer: ^3.3.3
    * coil-extension: @types/react-dom: ^16.9.3
    * coil-extension: @types/styled-components: ^4.1.20
    * coil-extension: @wext/shipit: ^0.2.1
    * coil-extension: addons-linter: ^1.15.3
    * coil-extension: @types/react: ^16.9.11
    * coil-oauth-scripts: @types/uuid: ^3.4.6
    * web-monetization-demo: @abraham/reflection: ^0.7.0
    * web-monetization-demo: @types/react-dom: ^16.9.3
    * web-monetization-demo: @types/react: ^16.9.11

--- packages/coil-monorepo-upkeep/src/commands/doCheckUpdates.ts
+++ packages/coil-monorepo-upkeep/src/commands/doCheckUpdates.ts
@@ -1,20 +1,42 @@
 import * as pathMod from 'path'
 
 import { cmd } from '../utils/cmd'
-import { fromRoot, getPackages, readFileJSON } from '../utils'
+import {
+  fromRoot,
+  getPackages,
+  pretty,
+  readFileJSON,
+  readPackageJSON
+} from '../utils'
 import { NcuRcJson } from '../types'
 
 const log = console.log
 
 export function doCheckUpdates() {
+  const rootPackageJSON = readPackageJSON(fromRoot('package.json'))
   const subPackages = getPackages({ withDependencies: false })
   const paths = [fromRoot('.')].concat(subPackages.map(li => li.location))
+
+  if (rootPackageJSON.resolutions) {
+    log('Using resolutions:')
+    Object.keys(rootPackageJSON.resolutions).forEach(k => {
+      if (rootPackageJSON.resolutions) {
+        const val = rootPackageJSON.resolutions[k]
+        log(`${k}: ${val}`)
+      }
+    })
+    log()
+  }
 
   const lines = ['feat: update dependencies', '']
   paths.forEach(path => {
     const packageFolder = pathMod.basename(path)
     const ncurc = readFileJSON<NcuRcJson>(pathMod.join(path, '.ncurc.json'))
-    log('Checking for updates for', packageFolder, 'rejecting', ncurc.reject)
+    log(
+      `Checking for updates for${packageFolder}, rejecting ${pretty(
+        ncurc.reject
+      )}`
+    )
     const upgraded = JSON.parse(cmd('npx ncu -u --jsonUpgraded', { cwd: path }))
     // * coil-extension: @types/puppeteer: ^1.19.0
     Object.keys(upgraded).forEach(k => {
