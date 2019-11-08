8829f4116bc19cc2ca828ebd157f02900b04a0d9
Nicholas Dudfield <ndudfield@gmail.com>
2019-11-08 16:40:04 +0700

    fix(coil-extension): re-enable youtube tests, fix #50

--- packages/coil-extension/test/puppeteer/basic-test.ts
+++ packages/coil-extension/test/puppeteer/basic-test.ts
@@ -8,11 +8,11 @@ import { testUrls } from './testUrls'
 
 async function run() {
   const { browser } = await initBrowserAndLoginFromEnv()
-  // const { success: youtubeRes } = await testMonetization({
-  //   browser,
-  //   url: testUrls.youtubeUrl
-  // })
-  // debug('Youtube Result=', youtubeRes)
+  const { success: youtubeRes } = await testMonetization({
+    browser,
+    url: testUrls.youtubeUrl
+  })
+  debug('Youtube Result=', youtubeRes)
   const { success: twitchRes } = await testMonetization({
     browser,
     url: testUrls.twitchUrl
--- packages/coil-extension/test/puppeteer/testUrls.ts
+++ packages/coil-extension/test/puppeteer/testUrls.ts
@@ -1,5 +1,5 @@
 const urls = {
-  // youtubeUrl: 'https://www.youtube.com/watch?v=-QMbZx_w2_Y',
+  youtubeUrl: 'https://www.youtube.com/watch?v=-QMbZx_w2_Y',
   twitchUrl: 'https://twitch.tv/vinesauce',
   coilArticle:
     'https://coil.com/p/thomasgardnerjr/Margaritaville-Jimmy-Buffett-cover-w-Sharper-s-Florist/dwntYG8d5'
