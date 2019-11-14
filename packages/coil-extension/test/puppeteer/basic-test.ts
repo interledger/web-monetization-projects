578f8b1e2d4a626351d3cf320b0779f6d0972d16
Nicholas Dudfield <ndudfield@gmail.com>
2019-11-14 12:31:38 +0700

    feat(coil-extension): disable youtube tests (#67)

--- packages/coil-extension/test/puppeteer/basic-test.ts
+++ packages/coil-extension/test/puppeteer/basic-test.ts
@@ -8,16 +8,17 @@ import { testUrls } from './testUrls'
 
 async function run() {
   const { browser } = await initBrowserAndLoginFromEnv()
-  const { success: youtubeRes } = await testMonetization({
-    browser,
-    url: testUrls.youtubeUrl
-  })
-  debug('Youtube Result=', youtubeRes)
+  // const { success: youtubeRes } = await testMonetization({
+  //   browser,
+  //   url: testUrls.youtubeUrl
+  // })
+  // debug('Youtube Result=', youtubeRes)
   const { success: twitchRes } = await testMonetization({
     browser,
     url: testUrls.twitchUrl
   })
   debug('Twitch Result=', twitchRes)
+  // noinspection ES6MissingAwait
   void browser.close()
   process.exit(/*youtubeRes && */ twitchRes ? 0 : 1)
 }
