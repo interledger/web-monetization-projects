e16c0b1b31fd50f54e6c47ff2cc738916f8ca690
Nicholas Dudfield <ndudfield@gmail.com>
2019-12-10 14:52:20 +0700

    refactor(coil-puppeteer-utils): decompose large file

---
+++ packages/coil-puppeteer-utils/src/lib/debug.ts
@@ -0,0 +1,3 @@
+import debugFactory from 'debug'
+
+export const debug = debugFactory('coil-puppeteer-utils:test')
---
+++ packages/coil-puppeteer-utils/src/lib/timeout.ts
@@ -0,0 +1,2 @@
+export const timeout = (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms))
