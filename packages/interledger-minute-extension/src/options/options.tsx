875c2e3a7357e64c81f816afc3e550ab3a24bcb8
Nicholas Dudfield <ndudfield@gmail.com>
2019-12-10 13:41:10 +0700

    feat(interledger-minute-extension): gut out and add react/material-ui

--- packages/interledger-minute-extension/src/options/options.tsx
+++ packages/interledger-minute-extension/src/options/options.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const rootEl = document.getElementById('root')
 const Index = () => {
-  return <h1> MINUTE EXTENSION POPUP </h1>
+  return <h1> MINUTE EXTENSION OPTIONS PAGE </h1>
 }
 
 ReactDOM.render(<Index />, rootEl)
