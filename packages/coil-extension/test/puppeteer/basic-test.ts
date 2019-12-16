aa21b49cd5b32e34af3df621f15b6f2d12924f6f
Nicholas Dudfield <ndudfield@gmail.com>
2019-12-17 06:07:03 +0700

    feat(coil-extension): reenable youtube tests (#160)

--- packages/coil-extension/test/puppeteer/basic-test.ts
+++ packages/coil-extension/test/puppeteer/basic-test.ts
@@ -8,11 +8,11 @@ import { testUrls } from './testUrls'
 
 async function run() {
   const { browser } = await initBrowserAndLoginFromEnv()
-  // const { success: youtubeRes } = await testMonetization({
-  //   browser,
-  //   url: testUrls.youtubeUrl
-  // })
-  // debug('Youtube Result=', youtubeRes)
+  const { success: youtubeRes } = await testMonetization({
+    browser,
+    url: testUrls.youtubeUrl
+  })
+  debug('Youtube Result=', youtubeRes)
   const { success: twitchRes } = await testMonetization({
     browser,
     url: testUrls.twitchUrl
@@ -20,7 +20,7 @@ async function run() {
   debug('Twitch Result=', twitchRes)
   // noinspection ES6MissingAwait
   void browser.close()
-  process.exit(/*youtubeRes && */ twitchRes ? 0 : 1)
+  process.exit(youtubeRes && twitchRes ? 0 : 1)
 }
 
 run().catch(e => {
