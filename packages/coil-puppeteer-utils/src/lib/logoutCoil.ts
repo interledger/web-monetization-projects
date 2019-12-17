faeaf489273b8b3761efdbe13aa7bde7fd3929f6
Nicholas Dudfield <ndudfield@gmail.com>
2019-12-17 12:23:21 +0700

    fix(coil-puppeteer-utils): use dynamic page for logout, fix #199

--- packages/coil-puppeteer-utils/src/lib/logoutCoil.ts
+++ packages/coil-puppeteer-utils/src/lib/logoutCoil.ts
@@ -1,12 +1,16 @@
 import { Page } from 'puppeteer'
 
 import { debug } from './debug'
+import { COIL_DOMAIN } from './env'
 
 export async function logoutCoil(coilPage: Page) {
   await coilPage.bringToFront()
+  await coilPage.goto(`${COIL_DOMAIN}/explore`)
 
   const menuSelector = `img[data-cy='hamburger-toggle']`
   const logoutSelector = `[data-cy='logout']`
+
+  await coilPage.waitFor(menuSelector)
 
   try {
     await coilPage.click(menuSelector)
@@ -17,7 +21,7 @@ export async function logoutCoil(coilPage: Page) {
       err
     )
   }
-
+  await coilPage.waitFor(logoutSelector)
   await coilPage.click(logoutSelector)
 
   return coilPage
