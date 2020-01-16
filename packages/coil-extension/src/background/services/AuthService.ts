b299a34e57eb2c74cc71232ebc2c671a96376423
Nicholas Dudfield <ndudfield@gmail.com>
2020-01-16 11:39:21 +0700

    feat(coil-extension): add auth logging (#283)

--- packages/coil-extension/src/background/services/AuthService.ts
+++ packages/coil-extension/src/background/services/AuthService.ts
@@ -7,6 +7,7 @@ import { LocalStorageProxy } from '../../types/storage'
 import * as tokens from '../../types/tokens'
 
 import { SiteToken } from './SiteToken'
+import { Logger, logger } from './utils'
 
 @injectable()
 export class AuthService extends EventEmitter {
@@ -14,6 +15,10 @@ export class AuthService extends EventEmitter {
     @inject(tokens.LocalStorageProxy)
     private store: LocalStorageProxy,
     private client: GraphQlClient,
+    @inject(tokens.CoilDomain)
+    private domain: string,
+    @logger('AuthService')
+    private log: Logger,
     private siteToken: SiteToken
   ) {
     super()
@@ -21,20 +26,26 @@ export class AuthService extends EventEmitter {
 
   async getTokenMaybeRefreshAndStoreState(): Promise<string | null> {
     let token = this.getStoredToken()
+    this.log('storedToken', { domain: this.domain, token })
 
     if (!token) {
       token = await this.siteToken.retrieve()
     }
+    this.log('siteToken', token)
 
     if (!token || tokenUtils.isExpired({ token })) {
       token = null
     } else if (tokenUtils.isExpired({ token, withinHrs: 12 })) {
       // Update the stored token/user
+      this.log('doing refreshTokenAndUpdateWhoAmi')
       token = await this.refreshTokenAndUpdateWhoAmi(token)
+      this.log('token after refreshTokenAndUpdateWhoAmi', token)
     } else {
       // Routinely do a whoami query to check for subscription status
       // Query could fail if token is invalid
+      this.log('doing updateWhoAmI', token)
       token = await this.updateWhoAmi(token)
+      this.log('token after updateWhoAmI', token)
     }
 
     if (token) {
@@ -50,6 +61,7 @@ export class AuthService extends EventEmitter {
 
   private async updateWhoAmi(token: string): Promise<string | null> {
     const resp = await this.client.whoAmI(token)
+    this.log('updateWhoAmi resp', resp.data)
     if (resp.data?.whoami) {
       this.store.user = resp.data.whoami
       return token
