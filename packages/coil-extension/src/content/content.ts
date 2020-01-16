133c54c2751c8907d63005fe58974ca4992e2a9d
Nicholas Dudfield <ndudfield@gmail.com>
2020-01-16 13:44:17 +0700

    feat(coil-extension): do not log queries in content page

--- packages/coil-extension/src/content/content.ts
+++ packages/coil-extension/src/content/content.ts
@@ -14,7 +14,12 @@ import { ContentScript } from './services/ContentScript'
 function configureContainer(container: Container) {
   container.bind(tokens.ContentRuntime).toConstantValue(API.runtime)
   container.bind(tokens.CoilDomain).toConstantValue(COIL_DOMAIN)
-  container.bind(tokens.Logger).toConstantValue(console.log)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const noop = (..._: unknown[]) => undefined
+  container
+    .bind(tokens.Logger)
+    .toConstantValue(noop)
+    .whenTargetNamed('CoilClient')
   container.bind(GraphQlClientOptions).to(ClientOptions)
   container.bind(Storage).toConstantValue(localStorage)
   container.bind(Window).toConstantValue(window)
