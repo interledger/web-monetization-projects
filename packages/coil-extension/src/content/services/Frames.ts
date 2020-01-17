d816c8483cdfd2af3479d113aee4256e2ed6ae6f
Nicholas Dudfield <ndudfield@gmail.com>
2020-01-17 19:36:40 +0700

    feat(coil-extension): "iframe[allow]" selector scribbles

--- packages/coil-extension/src/content/services/Frames.ts
+++ packages/coil-extension/src/content/services/Frames.ts
@@ -24,6 +24,10 @@ export class Frames {
     this.isIFrame = !this.isTopFrame
     this.isCoilTopFrame = this.isTopFrame && this.isAnyCoilFrame
     this.isDirectChild = this.isIFrame && window.parent === window.top
+
+    if (this.isDirectChild) {
+      console.log(this.window.frameElement?.getAttribute('id'))
+    }
   }
 
   monitor() {
@@ -37,6 +41,40 @@ export class Frames {
       }
       this.runtime.sendMessage(unload)
     })
+    this.window.addEventListener('message', message => {
+      console.log(
+        'window.message to',
+        window.location.href,
+        message.origin,
+        message.data
+      )
+    })
+    if (this.isTopFrame) {
+      setTimeout(() => {
+        // Can't really use *= or ~= because of potential monetization in a string etc
+        // need an allow syntax parser ....
+        const frames = document.querySelectorAll<HTMLIFrameElement>(
+          'iframe[allow]'
+        )
+        Array.from(frames).forEach((frame, ix) => {
+          // WOULD need to parse here ...
+          if (!frame.allow.match(/.*monetization.*/)) {
+            console.log('monetization not allowed', frame.allow)
+            return
+          }
+          const origin = new URL(frame.src).origin
+          console.log({ origin, hasWindow: Boolean(frame.contentWindow) })
+          frame.contentWindow?.postMessage(
+            {
+              // Need frameId/tabId
+              msg: `hello to iframe[@id="${frame.getAttribute('id')}"]`,
+              allow: frame.allow
+            },
+            origin
+          )
+        })
+      }, 1000)
+    }
   }
 
   private sendStateChange() {
