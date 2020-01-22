5292f055d9087f545309ffa7f276758b3d41683c
sentientwaffle <sentientwaffle@gmail.com>
2020-01-22 09:46:15 -0800

    fix(coil-extension): fix clock skew
    Ensure that payment succeeds in spite of a client's clock skew.
    
    Send all Prepare packets with a fixed `expiresAt` in the far future (`2100-01-01T00:00:00.000Z`). The connector clamps that using the `maxHoldTime` (30s) before forwarding the Prepare.
    
    Note that this includes https://github.com/interledgerjs/ilp-protocol-stream/pull/128 and https://github.com/interledgerjs/interledgerjs/pull/7.
    
    Fixes https://github.com/coilhq/coil/issues/2903.

--- packages/coil-extension/src/background/services/Stream.ts
+++ packages/coil-extension/src/background/services/Stream.ts
@@ -325,7 +325,8 @@ class StreamAttempt {
       plugin,
       slippage: 1.0,
       exchangeRate: 1.0,
-      maximumPacketAmount: '10000000'
+      maximumPacketAmount: '10000000',
+      getExpiry
     })
 
     if (!this._active) return
@@ -487,4 +488,14 @@ class StreamAttempt {
       }
     }
   }
+}
+
+// Use a fixed date in the distant future (2100-01-01T00:00:00.000Z) as the
+// expiry of all outgoing packets. This ensures that payment will work even
+// if the OS's clock is skewed. It will be replaced with a more reasonable
+// expiry by the connector.
+const FAR_FUTURE_EXPIRY = new Date(4102444800000)
+
+function getExpiry(destination: string): Date {
+  return FAR_FUTURE_EXPIRY
 }
