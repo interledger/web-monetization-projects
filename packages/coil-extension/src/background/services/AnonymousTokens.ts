c23cfe8de192b6bcd024b5e386b090ff62f3a40d
djg <sentientwaffle@gmail.com>
2020-01-31 10:21:50 -0800

    misc anonymous token updates (#286)
    * feat(coil-extension) anonymous tokens
    
    Open questions:
    
    * Right now to determine if a token is exhausted the extension checks for the substring `"exhausted capacity"` in the error message. Is this Good Enough or should a specific error code be selected?
    * What are the domains for the redeemer/signer services?
    
    * chore: code review
    
    * feat(extension): prefetch anonymous tokens
    
    See https://github.com/coilhq/web-monetization-projects/pull/286#discussion_r371445989
    
    * feat(extension): verify exhausted capacity
    
    See: https://github.com/coilhq/web-monetization-projects/pull/286#discussion_r372633177
    
    * chore: do upkeep
    
    Co-authored-by: Nicholas Dudfield <ndudfield@gmail.com>

---
+++ packages/coil-extension/src/background/services/AnonymousTokens.ts
@@ -0,0 +1,64 @@
+import { Container, decorate, inject, injectable } from 'inversify'
+import * as anonymousTokens from '@coil/anonymous-tokens'
+
+import * as tokens from '../../types/tokens'
+
+import { Logger, logger } from './utils'
+
+decorate(injectable(), anonymousTokens.AnonymousTokens)
+
+const ANON_TOKEN_BATCH_SIZE = 10
+
+@injectable()
+export class AnonymousTokens extends anonymousTokens.AnonymousTokens {
+  constructor(
+    @inject(tokens.CoilRedeemerUrl) redeemerUrl: string,
+    @inject(tokens.CoilSignerUrl) signerUrl: string,
+    @inject(Storage) storage: Storage,
+    @logger('AnonymousTokens') debug: Logger
+  ) {
+    super({
+      redeemerUrl,
+      signerUrl,
+      store: new TokenStore(storage),
+      debug,
+      batchSize: ANON_TOKEN_BATCH_SIZE
+    })
+  }
+}
+
+class TokenStore {
+  private storage: Storage
+  constructor(localStorage: Storage) {
+    this.storage = localStorage
+  }
+
+  setItem(key: string, value: string): Promise<string> {
+    this.storage.setItem(key, value)
+    return Promise.resolve(value)
+  }
+
+  removeItem(key: string): Promise<void> {
+    this.storage.removeItem(key)
+    return Promise.resolve()
+  }
+
+  iterate(
+    fn: (
+      value: string,
+      key: string,
+      iterationNumber: number
+    ) => anonymousTokens.SignedToken | undefined
+  ): Promise<anonymousTokens.SignedToken | undefined> {
+    let i = 0
+    let key
+    while ((key = this.storage.key(i)) !== null) {
+      const value = this.storage.getItem(key)
+      if (value === null) continue
+      const result = fn(value, key, i)
+      if (result) return Promise.resolve(result)
+      i++
+    }
+    return Promise.resolve(undefined)
+  }
+}
