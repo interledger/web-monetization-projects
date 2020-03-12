89f27d74262680229a534b5f01c552a7c7470b1b
sentientwaffle <sentientwaffle@gmail.com>
2020-03-12 10:10:20 -0700

    chore: remove dead code

--- packages/coil-anonymous-tokens/src/lib/privacypass/config.ts
+++ packages/coil-anonymous-tokens/src/lib/privacypass/config.ts
@@ -1,12 +1,3 @@
-// TODO: fix these storage functions
-export function get(_: string) {
-  return 'todo'
-}
-
-export function set(__: string, _: string) {
-  return
-}
-
 export function dev() {
   return true
 }
@@ -22,61 +13,4 @@ export function h2cParams() {
     hash: 'sha256',
     method: 'increment'
   }
-}
-
-export function requestIdentifiers() {
-  return {
-    'query-param': 'todo',
-    'post-processed': 'todo',
-    'body-param': 'todo'
-  }
-}
-
-export const sentTokens: any = {}
-
-export function tokensPerRequest() {
-  return 30
-}
-
-export function issueActionUrls() {
-  return []
-}
-
-export const LISTENER_URLS = 'todo'
-
-export function maxTokens() {
-  return 20
-}
-
-export const CHL_BYPASS_SUPPORT = 'todo'
-export function getConfigId() {
-  return 'todo'
-}
-
-export function signResponseFMT() {
-  return 'string'
-}
-
-export function storedCommitments() {
-  return {}
-}
-
-export function reloadOnSign() {
-  return false
-}
-
-export function chlCaptchaDomain() {
-  return 'todo'
-}
-
-export function setSpendFlag(_: any, __: any) {}
-
-export function updateBrowserTab(_: any, __: any) {}
-
-export function getConfigName() {
-  return 'todo'
-}
-
-export function getCommitmentsKey() {
-  return 'todo'
 }
