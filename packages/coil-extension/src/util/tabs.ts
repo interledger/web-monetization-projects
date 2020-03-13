d1cee671f7bc481a495091590fcf97f620102ee0
Nicholas Dudfield <ndudfield@gmail.com>
2020-03-13 15:21:55 +0700

    fix(coil-extension): do not use sender.tab.url

--- packages/coil-extension/src/util/tabs.ts
+++ packages/coil-extension/src/util/tabs.ts
@@ -2,12 +2,12 @@ import MessageSender = chrome.runtime.MessageSender
 import { notNullOrUndef } from './nullables'
 
 export function getTab(sender: { tab?: { id?: number } }): number {
-  return notNullOrUndef(notNullOrUndef(sender.tab).id)
+  return notNullOrUndef(notNullOrUndef(sender.tab, 'sender.tab').id, 'tab.id')
 }
 
 export function getFrameSpec(sender: MessageSender) {
   const tabId = getTab(sender)
-  const frameId = notNullOrUndef(sender.frameId)
+  const frameId = notNullOrUndef(sender.frameId, 'sender.frameId')
   const spec = { tabId, frameId }
   return { ...spec, spec }
 }
