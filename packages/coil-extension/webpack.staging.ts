8d7612f1ac88e5608eb0a7c5f424dfba517399dc
sentientwaffle <sentientwaffle@gmail.com>
2020-03-17 13:18:08 -0700

    chore: use staging.coil.com redeemer/issuer

--- packages/coil-extension/webpack.staging.ts
+++ packages/coil-extension/webpack.staging.ts
@@ -8,12 +8,18 @@ module.exports = merge(config, {
     new webpack.DefinePlugin({
       WEBPACK_DEFINE_COIL_DOMAIN: JSON.stringify('https://staging.coil.com'),
       // TODO use the real staging domains (not localhost)
-      //WEBPACK_DEFINE_COIL_REDEEMER: JSON.stringify('https://redeemer.staging.coil.com'),
-      //WEBPACK_DEFINE_COIL_SIGNER: JSON.stringify('https://signer.staging.coil.com')
+      WEBPACK_DEFINE_COIL_REDEEMER: JSON.stringify(
+        'https://staging.coil.com/redeemer'
+      ),
+      WEBPACK_DEFINE_COIL_SIGNER: JSON.stringify(
+        'https://staging.coil.com/issuer'
+      )
+      /*
       WEBPACK_DEFINE_COIL_REDEEMER: JSON.stringify(
         'http://localhost:8081/redeemer'
       ),
       WEBPACK_DEFINE_COIL_SIGNER: JSON.stringify('http://localhost:8080/issuer')
+      */
     })
   ]
 })
