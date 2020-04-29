9a80ca99548818a8c5be7e808a621f0daf19e03c
Nicholas Dudfield <ndudfield@gmail.com>
2020-04-29 16:45:12 +0700

    fix(ci): fix logout (#554)

--- packages/coil-puppeteer-utils/src/lib/logoutCoil.ts
+++ packages/coil-puppeteer-utils/src/lib/logoutCoil.ts
@@ -5,7 +5,7 @@ import { COIL_DOMAIN } from './env'
 
 export async function logoutCoil(coilPage: Page) {
   await coilPage.bringToFront()
-  await coilPage.goto(`${COIL_DOMAIN}/explore`)
+  await coilPage.goto(`${COIL_DOMAIN}/settings/account`)
 
   const menuSelector = `img[data-cy='hamburger-toggle']`
   const logoutSelector = `[data-cy='logout']`
