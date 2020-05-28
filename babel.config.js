f2abff178d620cfe29bf9659a4b508f8d042bc79
Nicholas Dudfield <ndudfield@gmail.com>
2020-05-28 15:32:30 +0700

    feat: update jest/ts-jest/babel-*

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 // babel.config.js
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-env', { targets: { node: '12' } }],
     '@babel/preset-typescript'
   ],
   plugins: [
+    ['@babel/proposal-decorators', { legacy: true }],
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread'
   ]
