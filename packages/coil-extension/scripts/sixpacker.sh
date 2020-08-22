84f5fe36fd4a99d33489ce11b0a8d46a733a9939
Nicholas Dudfield <ndudfield@gmail.com>
2020-08-22 14:32:47 +0700

    feat(coil-extension): add scripts/sixpacker.sh

---
+++ packages/coil-extension/scripts/sixpacker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -u
+# We use set -e and bash with -u to bail on first non zero exit code of any
+# processes launched or upon any unbound variable.
+# We use set -x to print commands before running them to help with
+# debugging.
+set -ex
+
+"$CHROME" --pack-extension=$PWD/dist \
+         --pack-extension-key=$COIL_SIX_PACK_KEY
+
+mv dist.crx six/assets
+
+pushd six
+jar cf ../coil-six.apk .
+popd
+
+jarsigner -verbose -keystore \
+  $COIL_SIX_KEYSTORE_PATH \
+  -storepass $COIL_SIX_KEYSTORE_PASS \
+  coil-six.apk coilkey
+
+echo 'To uninstall: "adb uninstall com.coil.android.six"'
+echo 'To install:   "adb install -r ./coil-six.apk"'
