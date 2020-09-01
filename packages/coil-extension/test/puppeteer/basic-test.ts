1c552db74cdd4361d4cb7b705e1b1a933ce0abeb
Nicholas Dudfield <ndudfield@gmail.com>
2020-09-01 16:49:47 +0700

    fix(playwright-utils): remove experiments

--- packages/coil-extension/test/puppeteer/basic-test.ts
+++ packages/coil-extension/test/puppeteer/basic-test.ts
@@ -10,18 +10,6 @@ import { testUrls } from './testUrls'
 async function run() {
   const { context } = await initBrowserAndLoginFromEnv()
 
-  await context.newPage()
-  await context.newPage()
-  await context.newPage()
-  await context.newPage()
-
-  // await page.goto('chrome://extensions')
-  // await page.click('#detailsButton')
-  // await page.click('extensions-toggle-row')
-  // await page.goto('chrome://extensions')
-  // await page.click('#devMode')
-  return
-
   let success = true
   for (const [name, url] of Object.entries(testUrls[env.COIL_DOMAIN])) {
     const { success: siteResult } = await testMonetization({
