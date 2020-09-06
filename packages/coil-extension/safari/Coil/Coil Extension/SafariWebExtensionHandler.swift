80f1675ef9a99d3038d109fca01c0a0ebdc0db6d
Nicholas Dudfield <ndudfield@gmail.com>
2020-09-06 17:00:00 +0700

    feat(coil-client): add safari project/doc/script (#995)
    * feat(coil-client): add safari project/doc/script
    
    * feat(coil-extension): add safari app icon

---
+++ packages/coil-extension/safari/Coil/Coil Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,24 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Coil Extension
+//
+//  Created by Nicholas  Dudfield on 9/6/20.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+        
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
