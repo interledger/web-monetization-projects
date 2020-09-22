79d430ac347133a1faeb83c7f292b324920b63f0
Nicholas Dudfield <ndudfield@gmail.com>
2020-09-22 17:05:04 +0700

    feat(coil-extension): support $x-local build directories (#1087)

--- packages/coil-extension/scripts/build-safari.sh
+++ packages/coil-extension/scripts/build-safari.sh
@@ -1,3 +1,4 @@
-xcodebuild build -project safari/Coil/Coil.xcodeproj
+SAFARI_DIR=${SAFARI_DIR:-safari}
+xcodebuild build -project "$SAFARI_DIR"/Coil/Coil.xcodeproj
 echo "check Safari (v14+) -> Preferences -> Extensions"
 echo "make sure to enable developer mode for extensions"
