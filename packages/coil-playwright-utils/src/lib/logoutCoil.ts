cdaef450a2d6645b618d87a0a8e05cb91314400f
Nicholas Dudfield <ndudfield@gmail.com>
2020-11-03 15:14:16 +0700

    fix(coil-playwright-utils): update logout routine, closes #1255

--- packages/coil-playwright-utils/src/lib/logoutCoil.ts
+++ packages/coil-playwright-utils/src/lib/logoutCoil.ts
@@ -1,24 +1,10 @@
 import { Page } from 'playwright'
 
-import { debug } from './debug'
 import { COIL_DOMAIN } from './env'
 
 export async function logoutCoil(coilPage: Page): Promise<Page> {
   await coilPage.bringToFront()
-  await coilPage.goto(`${COIL_DOMAIN}/settings/account`)
-
-  const menuSelector = `img[data-cy='hamburger-toggle']`
-  const logoutSelector = `[data-cy='logout']`
-
-  try {
-    await coilPage.click(menuSelector)
-  } catch (err) {
-    debug(
-      'Failed to open hamburger menu. ' +
-        'Page is likely wide enough to not render it. err=',
-      err
-    )
-  }
-  await coilPage.click(logoutSelector)
+  await coilPage.goto(`${COIL_DOMAIN}/settings`)
+  await coilPage.click('"Sign out"')
   return coilPage
 }
