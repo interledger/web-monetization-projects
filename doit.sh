af9cec541a07e17e4ff2e17c8e96e79f958c157f
Nicholas Dudfield <ndudfield@gmail.com>
2020-11-23 14:44:52 +0700

    chore(deps): update buffer requirement from ^6.0.2 to ^6.0.3
    Updates the requirements on [buffer](https://github.com/feross/buffer) to permit the latest version.
    - [Release notes](https://github.com/feross/buffer/releases)
    - [Commits](https://github.com/feross/buffer/compare/v6.0.2...v6.0.3)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>

---
+++ doit.sh
@@ -0,0 +1,7 @@
+update_wm_dependabot_branch dependabot/npm_and_yarn/buffer-tw-6.0.3
+update_wm_dependabot_branch dependabot/npm_and_yarn/debug-tw-4.3.1
+update_wm_dependabot_branch dependabot/npm_and_yarn/eslint-plugin-standard-5.0.0
+update_wm_dependabot_branch dependabot/npm_and_yarn/eslint-tw-7.14.0
+update_wm_dependabot_branch dependabot/npm_and_yarn/types/react-17.0.0
+update_wm_dependabot_branch dependabot/npm_and_yarn/types/react-dom-16.9.10
+update_wm_dependabot_branch dependabot/npm_and_yarn/web-ext-tw-5.4.0
