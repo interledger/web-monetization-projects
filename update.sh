a1d59a255fcec1c3b0c4ff2374dc5fdc154905d6
Nicholas Dudfield <ndudfield@gmail.com>
2020-12-02 12:53:15 +0700

    feat: remove scribble scripts

--- update.sh
+++
@@ -1,3 +0,0 @@
-update_wm_dependabot_branch dependabot/npm_and_yarn/babel/core-7.12.9
-update_wm_dependabot_branch dependabot/npm_and_yarn/material-ui/core-4.11.1
-update_wm_dependabot_branch dependabot/npm_and_yarn/material-ui/styles-4.11.1
