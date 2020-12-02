e4038b1cf2a795532cb16697fe851effc8dbc7c4
Nicholas Dudfield <ndudfield@gmail.com>
2020-12-02 12:54:55 +0700

    feat: remove scribble scripts (#1353)

--- done.sh
+++
@@ -1,3 +0,0 @@
-update_wm_dependabot_branch dependabot/npm_and_yarn/prettier-2.2.1
-update_wm_dependabot_branch dependabot/npm_and_yarn/ts-json-schema-generator-0.79.0
-update_wm_dependabot_branch dependabot/npm_and_yarn/webpack-5.8.0
