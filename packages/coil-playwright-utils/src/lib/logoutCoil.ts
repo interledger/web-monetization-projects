de9515fbb7a89693ca99ca74d99034acbba483d2
Nicholas Dudfield <ndudfield@gmail.com>
2021-02-09 14:33:44 +0700

    fix(ci): fix stalled logout (#1555)

--- packages/coil-playwright-utils/src/lib/logoutCoil.ts
+++ packages/coil-playwright-utils/src/lib/logoutCoil.ts
@@ -1,10 +1,25 @@
 import { Page } from 'playwright'
 
+import { debug } from './debug'
 import { COIL_DOMAIN } from './env'
 
 export async function logoutCoil(coilPage: Page): Promise<Page> {
   await coilPage.bringToFront()
   await coilPage.goto(`${COIL_DOMAIN}/settings`)
-  await coilPage.click('"Sign out"')
+
+  const menuSelector = `svg[data-cy='hamburger-toggle']`
+  const logoutSelector = `"Log out"`
+
+  try {
+    await coilPage.click(menuSelector, { delay: 5e3, clickCount: 1 })
+  } catch (err) {
+    debug(
+      'Failed to open hamburger menu. ' +
+        'Page is likely wide enough to not render it. err=',
+      err
+    )
+  }
+
+  await coilPage.click(logoutSelector)
   return coilPage
 }
