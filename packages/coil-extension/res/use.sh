0a3216b1a92856cf442146e2e910e4c42a768d8e
Nicholas Dudfield <ndudfield@gmail.com>
2021-02-19 12:15:17 +0700

    feat: update disabled icon to v4

--- packages/coil-extension/res/use.sh
+++
@@ -1,5 +0,0 @@
-cp icn-coil-ext@4x.png inactive.png
-cp icn-coil-ext-alert@4x.png monetized-unavailable.png
-cp icn-coil-ext-alert@4x.png inactive-unavailable.png
-cp icn-coil-ext-streaming@4x.png monetized-streaming.png
-cp icn-coil-ext-connecting@4x.png monetized.png
