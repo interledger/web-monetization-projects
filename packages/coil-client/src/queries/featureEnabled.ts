a9301e39aa54a4e26d301443cbbdff7e72aa25b7
Cairin Michie <cairinmichie@gmail.com>
2021-04-14 11:54:17 +0200

    feat: add featureEnabled query

---
+++ packages/coil-client/src/queries/featureEnabled.ts
@@ -0,0 +1,13 @@
+import { GraphQlClient } from '..'
+
+export interface FeatureEnabledData {
+  featureEnabled: boolean
+}
+
+export const featureEnabledQuery = `{
+  featureEnabled(key: $key)
+}`
+
+export async function featureEnabled(this: GraphQlClient, token: string, key: string) {
+  return this.query<FeatureEnabledData>({ query: featureEnabledQuery, token, variables: {key: key} })
+}
