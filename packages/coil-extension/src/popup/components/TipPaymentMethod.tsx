4f0170e0821742290f8c69b44669d8650aca022a
mwill8886 <13811701+mwill8886@users.noreply.github.com>
2021-04-13 20:14:55 -0600

    fix: format errors

--- packages/coil-extension/src/popup/components/TipPaymentMethod.tsx
+++ packages/coil-extension/src/popup/components/TipPaymentMethod.tsx
@@ -1,66 +1,70 @@
 import React from 'react'
 import { styled } from '@material-ui/core'
+
 import { Colors } from '../../shared-theme/colors'
+
 import { ITipView } from './views/TipRouter'
 
 //
 // Styles
 //
 const PaymentMethodWrapper = styled('div')({
-    fontWeight: 'normal',
-    color: Colors.Grey700,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  })
-  
-  const CardInfo = styled('div')({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    '& > img': {
-      margin: '0px 6px'
-    }
-  })
-  
-  const Dot = styled('div')({
-    backgroundColor: Colors.Grey700,
-    width: '5px',
-    height: '5px',
-    borderRadius: '5px',
-    marginRight: '2px',
-    '&:last-of-type': {
-      marginRight: '4px'
-    }
-  })
+  fontWeight: 'normal',
+  color: Colors.Grey700,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
+
+const CardInfo = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '& > img': {
+    margin: '0px 6px'
+  }
+})
+
+const Dot = styled('div')({
+  backgroundColor: Colors.Grey700,
+  width: '5px',
+  height: '5px',
+  borderRadius: '5px',
+  marginRight: '2px',
+  '&:last-of-type': {
+    marginRight: '4px'
+  }
+})
 
 //
 // Component
 //
-export const TipPaymentMethod = (props: {currentTipAmount: number}): React.ReactElement => {
-    const { currentTipAmount } = props;
+export const TipPaymentMethod = (props: {
+  currentTipAmount: number
+}): React.ReactElement => {
+  const { currentTipAmount } = props
 
-    const tipCreditBalance = 10 //! needs to be replaced with data from an api call to users settings
+  const tipCreditBalance = 10 //! needs to be replaced with data from an api call to users settings
 
-        if(tipCreditBalance >= currentTipAmount){
-        return (<PaymentMethodWrapper>Use tip credits: ${tipCreditBalance}</PaymentMethodWrapper>)
-        }
-        else{
-        return(
-            <PaymentMethodWrapper>
-                Pay with
-                <CardInfo>
-                    <img src='/res/creditcard-visa.svg' alt="visa icon" />
-                    <Dot />
-                    <Dot />
-                    <Dot />
-                    <Dot />
-                    3455
-                </CardInfo>
-            </PaymentMethodWrapper>
-        )
-    }
+  if (tipCreditBalance >= currentTipAmount) {
+    return (
+      <PaymentMethodWrapper>
+        Use tip credits: ${tipCreditBalance}
+      </PaymentMethodWrapper>
+    )
+  } else {
+    return (
+      <PaymentMethodWrapper>
+        Pay with
+        <CardInfo>
+          <img src='/res/creditcard-visa.svg' alt='visa icon' />
+          <Dot />
+          <Dot />
+          <Dot />
+          <Dot />
+          3455
+        </CardInfo>
+      </PaymentMethodWrapper>
+    )
+  }
 }
-
-
-
