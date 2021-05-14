e91412c85056e0875162759183484250058ec962
Nicholas Dudfield <ndudfield@gmail.com>
2021-05-14 12:07:27 +0700

    fix(coil-extension): squelch more logging

--- packages/coil-extension/src/background/services/PopupBrowserAction.ts
+++ packages/coil-extension/src/background/services/PopupBrowserAction.ts
@@ -36,6 +36,12 @@ export class PopupBrowserAction {
     // disable popup if on android
     try {
       this.api.runtime.getPlatformInfo(result => {
+        if (this.api.runtime.lastError) {
+          if (this.loggingEnabled) {
+            console.error(this.api.runtime.lastError)
+          }
+        }
+
         if (result?.os === 'android') {
           this.api.browserAction.setPopup({
             // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1426484
