7275256f510feeb0d221cd7321fceed3575fb20f
Nicholas Dudfield <ndudfield@gmail.com>
2021-06-04 11:03:59 +0700

    fix(coil-extension): fix comment

--- packages/coil-extension/src/background/services/BackgroundScript.ts
+++ packages/coil-extension/src/background/services/BackgroundScript.ts
@@ -660,7 +660,7 @@ export class BackgroundScript {
      * to be emitted.
      *
      * At the least, logging out (which will cause a stopped, finalized: false
-     * state) and then removal of the meta tag is a valid case of this
+     * state) and then removal of the meta tag is one valid case of this
      * transition so it's easier to simply track started -> pending here.
      *
      */
