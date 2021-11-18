51eaf46ac66c2149809472b0ac8a0aaff30fb5bd
Nicholas Dudfield <ndudfield@gmail.com>
2021-11-18 19:43:49 +0700

    feat: update polyfill hash

--- packages/webmonetization-wext/test/jest/content/wmPolyfill.test.ts
+++ packages/webmonetization-wext/test/jest/content/wmPolyfill.test.ts
@@ -4,7 +4,7 @@ import { wmPolyfill } from '@webmonetization/wext/content'
 
 describe('wmPolyfill', () => {
   it('should generate a known hash for the manifest', () => {
-    const knownHash = 'sha256-RGwX0iQYm3QeL7OlhtoWHXrcxXP+5Fpx80JaSJVqYRE='
+    const knownHash = 'sha256-UlD5z/lTi/41Xzy6oHP2oSWSPWXV7kKneIJMC8Rs4IE='
     const data = Buffer.from(wmPolyfill, 'utf-8')
     const digest = createHash('sha256').update(data).digest()
     const rebuilt = `sha256-${digest.toString('base64')}`
