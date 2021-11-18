0a7f123c6432b5a5f88aed82ed5727fa7957d477
Nicholas Dudfield <ndudfield@gmail.com>
2021-11-18 20:20:28 +0700

    feat: fix existing link branch

--- packages/webmonetization-wext/test/jest/content/wmPolyfill.test.ts
+++ packages/webmonetization-wext/test/jest/content/wmPolyfill.test.ts
@@ -4,7 +4,7 @@ import { wmPolyfill } from '@webmonetization/wext/content'
 
 describe('wmPolyfill', () => {
   it('should generate a known hash for the manifest', () => {
-    const knownHash = 'sha256-UlD5z/lTi/41Xzy6oHP2oSWSPWXV7kKneIJMC8Rs4IE='
+    const knownHash = 'sha256-tP7pJaxb+RqvK3uuVUvfX28qdmh+X2QLd55TMmzlmxI='
     const data = Buffer.from(wmPolyfill, 'utf-8')
     const digest = createHash('sha256').update(data).digest()
     const rebuilt = `sha256-${digest.toString('base64')}`
