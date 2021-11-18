85e278274bc2cf5ea80ee301bb5aed7cf67d4387
Nicholas Dudfield <ndudfield@gmail.com>
2021-11-18 19:38:46 +0700

    feat: experiment with Symbol.toStringTag and pointer prop

--- packages/webmonetization-wext/src/content/wmPolyfill.ts
+++ packages/webmonetization-wext/src/content/wmPolyfill.ts
@@ -22,17 +22,72 @@ const basicEventsLoggingCode = createBindingCode(
 // This can be quite noisy so only enable when localStorage.WM_DEBUG_PROGRESS
 // is set.
 const progressLoggingCode = createBindingCode('monetizationprogress')
-
 // language=JavaScript
 export const wmPolyFillMinimal = `
-  navigator.monetization = new EventTarget()
-  navigator.monetization.state = 'idle'
+  const selector = 'link[rel="monetization"]'
 
-  document.monetization = document.createElement('div')
+  class Monetization extends EventTarget {
+    state = 'idle'
+
+    get [Symbol.toStringTag]() {
+      return 'Monetization'
+    }
+
+
+    toJSON() {
+      const json = {state: this.state}
+      if (Monetization.prototype.hasOwnProperty('pointer')) {
+        json['pointer'] = this.pointer
+      }
+      return json
+    }
+  }
+
+  if (localStorage.WM_POINTER_PROPERTY) {
+    Object.defineProperty(Monetization.prototype, 'pointer', {
+      enumerable: true,
+      configurable: true,
+      writeable: true,
+        get() {
+          const link = document.querySelector(selector)
+          if (link && link.hasAttribute('href')) {
+            return link.getAttribute('href')
+          }
+          return null
+        },
+
+        set(val) {
+          // Hello PPK :)
+          // NOTE: document.title will edit an existing <title> tag, and
+          // append a <title> if none exist
+          if (val === null) {
+            const existing = document.querySelector(selector)
+            if (existing) {
+              existing.remove()
+            }
+          } else if (typeof val !== 'string') {
+            throw new Error('pointer must be https:// url string')
+          } else {
+            let link = document.querySelector(selector)
+            if (!link) {
+              link = document.createElement('link')
+              link.setAttribute('rel', 'monetization')
+              link.setAttribute('href', val)
+              document.head.appendChild(link)
+            }
+          }
+        }
+      }
+    )
+  }
+
+  navigator.monetization = new Monetization()
+
+  document.monetization = new EventTarget()
   document.monetization.state = 'stopped'
 
   document.addEventListener('monetization-v1', function(event) {
-    const {type, detail} = event.detail
+    const { type, detail } = event.detail
     if (type === 'monetizationstatechange') {
       // document.monetization
       document.monetization.state = detail.state
@@ -41,21 +96,20 @@ export const wmPolyFillMinimal = `
       const oldState = navigator.monetization.state
 
       let newState
+      // TODO: when exactly is interactive ???
       if (detail.state === 'started') {
         newState = 'interactive'
       }
-      if (detail.state === 'pending') {
-        newState = 'interactive'
-      }
+      // if (detail.state === 'pending') {
+      //   newState = 'interactive'
+      // }
       if (detail.state === 'stopped') {
         newState = 'idle'
       }
-      if (newState !== oldState) {
+      if (newState && (newState !== oldState)) {
         navigator.monetization.state = newState
-        navigator.monetization.dispatchEvent(new CustomEvent('statechange', {detail: {
-          state: newState,
-          previous: oldState
-          }}))
+        const stateChange = new CustomEvent('statechange')
+        navigator.monetization.dispatchEvent(stateChange)
       }
     } else {
       document.monetization.dispatchEvent(
@@ -64,7 +118,7 @@ export const wmPolyFillMinimal = `
         }))
       if (type === 'monetizationprogress') {
         navigator.monetization.dispatchEvent(
-          new CustomEvent('monetization', {detail: detail}))
+          new CustomEvent('monetization', { detail: detail }))
       }
     }
   })
@@ -76,6 +130,11 @@ export const wmPolyfill = `
 
   if (localStorage.WM_DEBUG) {
     ${basicEventsLoggingCode}
+    navigator.monetization.addEventListener('statechange', (e) => {
+      console.log(
+        '%c Web-Monetization-2 statechange event: %s',
+        'color: aqua; background-color: black', navigator.monetization.state)
+    })
   }
 
   if (localStorage.WM_DEBUG_PROGRESS) {
