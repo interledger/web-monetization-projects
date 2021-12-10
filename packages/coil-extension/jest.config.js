de15dbab40840e0022d15dbf4a76c52fe0e6c387
Chris Jackson <chris.jackson@coil.com>
2021-12-10 13:46:51 -0800

    test: upkeep update jest.config.js files

--- packages/coil-extension/jest.config.js
+++ packages/coil-extension/jest.config.js
@@ -1,6 +1,16 @@
 // FROM UPKEEP TEMPLATE
 module.exports = {
   ...require('../../jest.config'),
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/test/jest/**/*.test.[jt]s?(x)']
+  testMatch: ['<rootDir>/test/jest/**/*.test.[jt]s?(x)'],
+
+  // coverage
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: 'results/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    // additional directories not under "src"
+    'assets/**/*.{js,jsx,ts,tsx}',
+    'polyfills/**/*.{js,jsx,ts,tsx}'
+  ]
 }
