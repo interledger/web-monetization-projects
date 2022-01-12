bbd38849f066cfeb809abaa21ec695e4e19fcc04
mwill8886 <13811701+mwill8886@users.noreply.github.com>
2022-01-12 14:07:45 -0700

    feat: stubbed in logic to disable tipping navigation

--- packages/coil-extension/src/popup/components/NavBar.tsx
+++ packages/coil-extension/src/popup/components/NavBar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box, styled, Theme } from '@material-ui/core'
 import SettingsIcon from '@material-ui/icons/Settings'
 
+import { useStore } from '../context/storeContext'
 import { useRouter } from '../context/routerContext'
 import { ROUTES } from '../constants'
 
@@ -12,26 +13,28 @@ import { Gift } from './icons/Gift'
 // Styles
 //
 
-const NavButton = styled('div')(({ theme }: { theme: Theme }) => ({
-  cursor: 'pointer',
-  flex: '1',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: '19px 0px',
-  color: theme.palette.Grey200,
-  '&:hover, &.active': {
-    '&:nth-child(1)': {
-      color: theme.palette.Green700
-    },
-    '&:nth-child(2)': {
-      color: theme.palette.Violet700
-    },
-    '&:last-child': {
-      color: theme.palette.Grey800
+const NavButton = styled('div')(
+  ({ theme, disabled }: { theme: Theme; disabled: boolean }) => ({
+    cursor: 'pointer',
+    flex: '1',
+    display: disabled ? 'none' : 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '19px 0px',
+    color: theme.palette.Grey200,
+    '&:hover, &.active': {
+      '&:nth-child(1)': {
+        color: theme.palette.Green700
+      },
+      '&:nth-child(2)': {
+        color: theme.palette.Violet700
+      },
+      '&:last-child': {
+        color: theme.palette.Grey800
+      }
     }
-  }
-}))
+  })
+)
 
 //
 // Component
@@ -39,6 +42,16 @@ const NavButton = styled('div')(({ theme }: { theme: Theme }) => ({
 
 export const NavBar = () => {
   const router = useRouter()
+  const { user } = useStore()
+  const { tipCredits, tipSettings: { inTippingBeta = false } = {} } = user ?? {}
+
+  const newUiFeatureFlag = true //todo: need to replace with background api call
+
+  // this might cause some odd rendering side effects once the user has completed a tip that would
+  // drain their tipCredit balance and revoke their eligibility to use tipping
+  const canTip =
+    newUiFeatureFlag && ((!inTippingBeta && tipCredits > 0) || inTippingBeta)
+
   return (
     <Box display='flex'>
       <NavButton
@@ -47,8 +60,8 @@ export const NavBar = () => {
       >
         <WebMonetized />
       </NavButton>
-      {/* Need to chec if the user has tipping enabled to dsiplay the tipping button */}
       <NavButton
+        disabled={!canTip}
         className={router.path.includes('tipping') ? 'active' : ''}
         onClick={() => router.to(ROUTES.tipping)}
       >
