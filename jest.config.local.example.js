9a846eefbe58fc15082a87779b8d3357dbefd25c
Nicholas Dudfield <ndudfield@gmail.com>
2022-01-17 16:16:55 +0700

    fix: fix jest/swc builds

--- jest.config.local.example.js
+++ jest.config.local.example.js
@@ -1,11 +1,7 @@
-// require('@abraham/reflection')
+// TODO:swc
+require('@abraham/reflection')
 
 module.exports = config => {
-  if (!process.env.IGNORE_LOCAL_JEST) {
-    // require abraham reflection
-    // config.setupFiles = [__filename]
-    // console.log('jest.config.local.js: Overriding typescript, and using babel')
-    delete config.preset
-  }
+  config.setupFiles = [__filename]
   return config
 }
