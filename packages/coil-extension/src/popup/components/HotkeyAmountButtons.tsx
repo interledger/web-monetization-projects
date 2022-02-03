30e19a119657f4101ee145cb24d73338a219e43a
mwill8886 <13811701+mwill8886@users.noreply.github.com>
2022-02-03 14:56:12 -0700

    feat: chagned behavior of hotkeys to no longer forward step

--- packages/coil-extension/src/popup/components/HotkeyAmountButtons.tsx
+++ packages/coil-extension/src/popup/components/HotkeyAmountButtons.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { styled } from '@material-ui/core'
+import { styled, Theme } from '@material-ui/core'
 
-import { Colors } from '../../shared-theme/colors'
 import { useStore } from '../context/storeContext'
 import { useTip } from '../context/tipContext'
-import { useRouter } from '../context/routerContext'
-import { ROUTES } from '../constants'
 
 //
 // Styles
@@ -16,50 +13,52 @@ const HotkeyButtonsWrapper = styled('div')({
   justifyContent: 'space-between'
 })
 
-const HotkeyButton = styled('button')({
-  cursor: 'pointer',
-  border: 'none',
-  borderRadius: '100px',
-  backgroundColor: Colors.Violet10,
-  color: Colors.Violet700,
-  fontFamily: 'CircularStd',
-  fontWeight: 'bold',
-  fontSize: '16px',
-  letterSpacing: '.5px',
-  textAlign: 'center',
-  padding: '10px 0px',
-  width: '54px',
-  flex: '1',
-  '&:hover': {
-    backgroundColor: Colors.Violet400,
-    color: '#FFFFFF'
-  },
-  '&:active': {
-    backgroundColor: Colors.Violet700,
-    color: '#FFFFFF'
-  },
-  '&:disabled': {
-    cursor: 'not-allowed',
-    backgroundColor: Colors.Grey50,
-    color: Colors.Grey100
-  },
-  '&:nth-child(2)': {
-    margin: '0px 15px'
-  }
-})
+const HotkeyButton = styled('button')(
+  ({ theme, active }: { theme: Theme; active: boolean }) => ({
+    cursor: 'pointer',
+    border: 'none',
+    borderRadius: '100px',
+    backgroundColor: active ? theme.palette.Violet700 : theme.palette.Violet10,
+    color: active ? '#FFFFFF' : theme.palette.Violet700,
+    fontFamily: 'CircularStd',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    letterSpacing: '.5px',
+    textAlign: 'center',
+    padding: '10px 0px',
+    width: '54px',
+    flex: '1',
+    '&:hover': {
+      backgroundColor: active
+        ? theme.palette.Violet700
+        : theme.palette.Violet400,
+      color: '#FFFFFF'
+    },
+    '&:active': {
+      backgroundColor: theme.palette.Violet700,
+      color: '#FFFFFF'
+    },
+    '&:disabled': {
+      cursor: 'not-allowed',
+      backgroundColor: theme.palette.Grey50,
+      color: theme.palette.Grey100
+    },
+    '&:nth-child(2)': {
+      margin: '0px 15px'
+    }
+  })
+)
 
 //
 // Component
 //
 export const HotkeyAmountButtons = (): React.ReactElement => {
-  const router = useRouter()
   const { user } = useStore()
   const { hotkeyTipAmountsUsd } = user?.tipSettings || {}
-  const { setCurrentTipAmountUsd, maxAllowableTipAmountUsd } = useTip()
+  const { setCurrentTipAmountUsd, maxAllowableTipAmountUsd, currentTipAmountUsd } = useTip()
 
   const handleSelectAmount = (amountUsd: number) => {
     setCurrentTipAmountUsd(amountUsd)
-    router.to(ROUTES.tippingConfirm)
   }
   return (
     <HotkeyButtonsWrapper>
@@ -67,6 +66,7 @@ export const HotkeyAmountButtons = (): React.ReactElement => {
         return (
           <HotkeyButton
             key={`pdt-${index}`}
+            active={amountUsd === currentTipAmountUsd}
             disabled={amountUsd > maxAllowableTipAmountUsd}
             onClick={() => handleSelectAmount(amountUsd)}
           >
