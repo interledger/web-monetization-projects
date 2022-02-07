b5b90b8240baf87caacc7d719c03a2fae3176048
mwill8886 <13811701+mwill8886@users.noreply.github.com>
2022-02-07 16:00:40 -0700

    fix: update tip credit balance and final tip amount when local storage updates

--- packages/coil-extension/src/popup/context/tipContext.tsx
+++ packages/coil-extension/src/popup/context/tipContext.tsx
@@ -1,6 +1,7 @@
-import React, { createContext, useContext, useState, useMemo } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 import { StorageService } from '@webmonetization/wext/services'
 
+import { PopupHost } from '../types'
 import { getCreditCardFromPaymentMethods } from '../../util/getCreditCardFromPaymentMethods'
 import { calculateMaxAllowableTip } from '../../util/calculateMaxAllowableTip'
 
@@ -9,13 +10,18 @@ import { calculateMaxAllowableTip } from '../../util/calculateMaxAllowableTip'
 //
 interface ITipContext {
   currentTipAmountUsd: number
+  finalTipAmountUsd: number //* only used for the TipCompleteView so it renders the proper amount after local storage updates
   maxAllowableTipAmountUsd: number //* the maxAllowableTip is primarily responsible for disabling tipping inputs
   setCurrentTipAmountUsd: (amount: number) => void
+  setFinalTipAmountUsd: (amount: number) => void
 }
 
 interface ITipProvider {
   storage: Pick<StorageService, 'get'>
+  host: PopupHost
 }
+
+type StorageEventPartial = Pick<StorageEvent, 'key' | 'newValue'>
 
 //
 // Context
@@ -26,53 +32,72 @@ const TipContext = createContext({} as ITipContext)
 // Provider
 //
 export const TipProvider: React.FC<ITipProvider> = props => {
-  const { storage, children } = props
-
-  const userObject = storage.get('user')
-  const monetized = storage.get('monetized')
-
-  const {
-    tipSettings: {
-      lastTippedAmountUsd = 0,
-      totalTipCreditAmountUsd = 0,
-      limitRemainingAmountUsd = 0,
-      minTipLimitAmountUsd = 1
-    } = {},
-    paymentMethods,
-    tippingBetaFeatureFlag
-  } = userObject ?? {}
-
-  const creditCard = getCreditCardFromPaymentMethods(paymentMethods)
-
-  const maxTipAllowedUsd = calculateMaxAllowableTip(
-    monetized,
-    tippingBetaFeatureFlag,
-    !!creditCard,
-    totalTipCreditAmountUsd,
-    limitRemainingAmountUsd
-  )
-
-  let initialTipAmountUsd = minTipLimitAmountUsd
-  if (lastTippedAmountUsd > 0) {
-    if (maxTipAllowedUsd < lastTippedAmountUsd) {
-      initialTipAmountUsd = maxTipAllowedUsd
-    } else {
-      initialTipAmountUsd = lastTippedAmountUsd
-    }
-  }
-  if (maxTipAllowedUsd === 0) {
-    initialTipAmountUsd = maxTipAllowedUsd
-  }
-
-  const [currentTipAmountUsd, setCurrentTipAmountUsd] =
-    useState<number>(initialTipAmountUsd)
+  const { storage, host, children } = props
+  const [currentTipAmountUsd, setCurrentTipAmountUsd] = useState<number>(0)
+  const [finalTipAmountUsd, setFinalTipAmountUsd] = useState<number>(0)
   const [maxAllowableTipAmountUsd, setMaxAllowableTipAmountUsd] =
-    useState<number>(maxTipAllowedUsd)
+    useState<number>(0)
+
+  // Gets the values from localstorage and updates the tipping settings based on the returned values
+  const setValuesFromStorage = () => {
+    const userObject = storage.get('user')
+    const monetized = storage.get('monetized')
+
+    const {
+      tipSettings: {
+        lastTippedAmountUsd = 0,
+        totalTipCreditAmountUsd = 0,
+        limitRemainingAmountUsd = 0,
+        minTipLimitAmountUsd = 1
+      } = {},
+      paymentMethods,
+      tippingBetaFeatureFlag
+    } = userObject ?? {}
+
+    const creditCard = getCreditCardFromPaymentMethods(paymentMethods)
+
+    const maxTipAllowedUsd = calculateMaxAllowableTip(
+      monetized,
+      tippingBetaFeatureFlag,
+      !!creditCard,
+      totalTipCreditAmountUsd,
+      limitRemainingAmountUsd
+    )
+
+    let initialTipAmountUsd = minTipLimitAmountUsd
+    if (lastTippedAmountUsd > 0) {
+      if (maxTipAllowedUsd < lastTippedAmountUsd) {
+        initialTipAmountUsd = maxTipAllowedUsd
+      } else {
+        initialTipAmountUsd = lastTippedAmountUsd
+      }
+    }
+    if (maxTipAllowedUsd === 0) {
+      initialTipAmountUsd = maxTipAllowedUsd
+    }
+
+    setCurrentTipAmountUsd(initialTipAmountUsd)
+    setMaxAllowableTipAmountUsd(maxTipAllowedUsd)
+  }
+
+  useEffect(() => {
+    setValuesFromStorage() // set the initial values
+
+    // need to update the values whenever the store user object is updated
+    const events = host.events
+    events.on('storage', (evt: StorageEventPartial) => {
+      if (evt.key === 'user') {
+        setValuesFromStorage()
+      }
+    })
+  }, [])
 
   const providerValue = {
     currentTipAmountUsd: currentTipAmountUsd,
+    finalTipAmountUsd: finalTipAmountUsd,
     maxAllowableTipAmountUsd: maxAllowableTipAmountUsd,
-    setCurrentTipAmountUsd: setCurrentTipAmountUsd
+    setCurrentTipAmountUsd: setCurrentTipAmountUsd,
+    setFinalTipAmountUsd: setFinalTipAmountUsd
   }
 
   return (
