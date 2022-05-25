e70197459bda1ff6a96a9494613440845ef597d5
Nicholas Dudfield <ndudfield@gmail.com>
2022-05-25 11:05:20 +0700

    feat(deps): update TypeScript to 4.7.2 (#2870)
    * feat(deps): update TypeScript to 4.7.2
    
    * fix: run yarn dedupe, update sdks

--- packages/coil-client/src/utils/portableFetch.ts
+++ packages/coil-client/src/utils/portableFetch.ts
@@ -1,15 +1,3 @@
 // node-fetch will use window.fetch if in the browser
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const fetchFn: typeof fetch = require('node-fetch')
-
-/**
- * Works in browser (including firefox/chrome extensions) and node
- * Don't use @types/node-fetch as we want to set credentials/cache
- * settings.
- */
-export async function portableFetch(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<Response> {
-  return fetchFn(input, init)
-}
+export const portableFetch: typeof fetch = require('node-fetch')
