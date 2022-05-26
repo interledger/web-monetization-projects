4cc7840055cc5846afa8c8399cdf407b6f87c35d
Nicholas Dudfield <ndudfield@gmail.com>
2022-05-26 08:12:23 +0700

    fix: update portableFetch impl for TS 3.7.2

--- packages/coil-client/src/utils/portableFetch.ts
+++ packages/coil-client/src/utils/portableFetch.ts
@@ -1,3 +1,6 @@
 // node-fetch will use window.fetch if in the browser
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const portableFetch: typeof fetch = require('node-fetch')
+export const portableFetch: typeof window.fetch =
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('node-fetch')
+    // we must bind to window if we set fetch as a class property
+    .bind(window)
