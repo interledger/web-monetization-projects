83d257fc2ef80aefd0c28d4e353fb21a950e5213
Nicholas Dudfield <ndudfield@gmail.com>
2022-06-09 14:23:32 +0700

    feat: noop bg/content for publishing experimment

--- packages/coil-extension/src/background/background.ts
+++ packages/coil-extension/src/background/background.ts
@@ -1,103 +1,105 @@
-// import '@abraham/reflection'
-// import { Container } from 'inversify'
-// import { GraphQlClient } from '@coil/client'
-export const dbg = console.log
+console.log('background.ts')
 
-const bgPage = Math.random()
-
-dbg('background page ok26')
-
-const ws = new WebSocket('ws://localhost:4444')
-ws.addEventListener('message', message => {
-  dbg('got a message')
-  const parsed = JSON.parse(message.data)
-  chrome.runtime.reload()
-})
-
-setInterval(() => {
-  console.log('interval from bg page', bgPage)
-}, 5e3)
-
-// import { makeLoggerMiddleware } from 'inversify-logger-middleware'
+// // import '@abraham/reflection'
+// // import { Container } from 'inversify'
+// // import { GraphQlClient } from '@coil/client'
+// export const dbg = console.log
 //
-// import { API, BUILD_CONFIG, COIL_DOMAIN, VERSION } from '../webpackDefines'
-// import { StorageService } from '../services/storage'
-// import * as tokens from '../types/tokens'
-// import { ClientOptions } from '../services/ClientOptions'
-// import { decorateThirdPartyClasses } from '../services/decorateThirdPartyClasses'
-// import { loggingEnabled } from '../util/isLoggingEnabled'
+// const bgPage = Math.random()
 //
-// import { BackgroundScript } from './services/BackgroundScript'
-// import { BackgroundStorageService } from './services/BackgroundStorageService'
-// import { Stream } from './services/Stream'
-// import { createLogger } from './services/utils'
+// dbg('background page ok26')
 //
-// async function configureContainer(container: Container) {
-//   if (loggingEnabled) {
-//     const logger = makeLoggerMiddleware()
-//     container.applyMiddleware(logger)
-//   }
+// const ws = new WebSocket('ws://localhost:4444')
+// ws.addEventListener('message', message => {
+//   dbg('got a message')
+//   const parsed = JSON.parse(message.data)
+//   chrome.runtime.reload()
+// })
 //
-//   container.bind(tokens.CoilDomain).toConstantValue(COIL_DOMAIN)
-//   container.bind(tokens.UserAgent).toConstantValue(navigator.userAgent)
-//   container.bind(tokens.WextApi).toConstantValue(API)
-//   container.bind(tokens.BuildConfig).toConstantValue(BUILD_CONFIG)
-//   container.bind(tokens.LoggingEnabled).toConstantValue(loggingEnabled)
-//   container.bind(GraphQlClient.Options).to(ClientOptions)
-//   container.bind(Storage).toConstantValue({})
-//   container.bind(StorageService).to(BackgroundStorageService)
-//   container.bind(Container).toConstantValue(container)
+// setInterval(() => {
+//   console.log('interval from bg page', bgPage)
+// }, 5e3)
 //
-//   container.bind(Stream).toSelf().inTransientScope()
-//
-//   container
-//     .bind(tokens.NoContextLoggerName)
-//     .toConstantValue('tokens.NoContextLoggerName')
-//
-//   container.bind(tokens.Logger).toDynamicValue(createLogger).inTransientScope()
-//
-//   container.bind(tokens.LocalStorageProxy).toDynamicValue(context => {
-//     return context.container.get(StorageService).makeProxy(['token'])
-//   })
-// }
-//
-// declare global {
-//   interface Window {
-//     bg: BackgroundScript
-//     clearTokens: () => void
-//     clearPopupRouteState: () => void
-//   }
-// }
-//
-// function prefixClearer(prefix: string) {
-//   return () => {
-//     for (let i = 0; i < localStorage.length; i++) {
-//       const key = localStorage.key(i)
-//       if (key?.startsWith(prefix)) {
-//         console.log('deleting', key)
-//         localStorage.removeItem(key)
-//       }
-//     }
-//   }
-// }
-//
-// window.clearPopupRouteState = prefixClearer('popup-route:')
-// window.clearTokens = prefixClearer('anonymous_token:')
-//
-// async function main() {
-//   if (loggingEnabled) {
-//     console.log('Loading Coil extension:', JSON.stringify(VERSION))
-//   }
-//   decorateThirdPartyClasses()
-//
-//   const container = new Container({
-//     defaultScope: 'Singleton',
-//     autoBindInjectable: true
-//   })
-//
-//   await configureContainer(container)
-//   window.bg = container.get(BackgroundScript)
-//   void window.bg.run()
-// }
-//
-// main().catch(console.error)
+// // import { makeLoggerMiddleware } from 'inversify-logger-middleware'
+// //
+// // import { API, BUILD_CONFIG, COIL_DOMAIN, VERSION } from '../webpackDefines'
+// // import { StorageService } from '../services/storage'
+// // import * as tokens from '../types/tokens'
+// // import { ClientOptions } from '../services/ClientOptions'
+// // import { decorateThirdPartyClasses } from '../services/decorateThirdPartyClasses'
+// // import { loggingEnabled } from '../util/isLoggingEnabled'
+// //
+// // import { BackgroundScript } from './services/BackgroundScript'
+// // import { BackgroundStorageService } from './services/BackgroundStorageService'
+// // import { Stream } from './services/Stream'
+// // import { createLogger } from './services/utils'
+// //
+// // async function configureContainer(container: Container) {
+// //   if (loggingEnabled) {
+// //     const logger = makeLoggerMiddleware()
+// //     container.applyMiddleware(logger)
+// //   }
+// //
+// //   container.bind(tokens.CoilDomain).toConstantValue(COIL_DOMAIN)
+// //   container.bind(tokens.UserAgent).toConstantValue(navigator.userAgent)
+// //   container.bind(tokens.WextApi).toConstantValue(API)
+// //   container.bind(tokens.BuildConfig).toConstantValue(BUILD_CONFIG)
+// //   container.bind(tokens.LoggingEnabled).toConstantValue(loggingEnabled)
+// //   container.bind(GraphQlClient.Options).to(ClientOptions)
+// //   container.bind(Storage).toConstantValue({})
+// //   container.bind(StorageService).to(BackgroundStorageService)
+// //   container.bind(Container).toConstantValue(container)
+// //
+// //   container.bind(Stream).toSelf().inTransientScope()
+// //
+// //   container
+// //     .bind(tokens.NoContextLoggerName)
+// //     .toConstantValue('tokens.NoContextLoggerName')
+// //
+// //   container.bind(tokens.Logger).toDynamicValue(createLogger).inTransientScope()
+// //
+// //   container.bind(tokens.LocalStorageProxy).toDynamicValue(context => {
+// //     return context.container.get(StorageService).makeProxy(['token'])
+// //   })
+// // }
+// //
+// // declare global {
+// //   interface Window {
+// //     bg: BackgroundScript
+// //     clearTokens: () => void
+// //     clearPopupRouteState: () => void
+// //   }
+// // }
+// //
+// // function prefixClearer(prefix: string) {
+// //   return () => {
+// //     for (let i = 0; i < localStorage.length; i++) {
+// //       const key = localStorage.key(i)
+// //       if (key?.startsWith(prefix)) {
+// //         console.log('deleting', key)
+// //         localStorage.removeItem(key)
+// //       }
+// //     }
+// //   }
+// // }
+// //
+// // window.clearPopupRouteState = prefixClearer('popup-route:')
+// // window.clearTokens = prefixClearer('anonymous_token:')
+// //
+// // async function main() {
+// //   if (loggingEnabled) {
+// //     console.log('Loading Coil extension:', JSON.stringify(VERSION))
+// //   }
+// //   decorateThirdPartyClasses()
+// //
+// //   const container = new Container({
+// //     defaultScope: 'Singleton',
+// //     autoBindInjectable: true
+// //   })
+// //
+// //   await configureContainer(container)
+// //   window.bg = container.get(BackgroundScript)
+// //   void window.bg.run()
+// // }
+// //
+// // main().catch(console.error)
--- packages/coil-extension/src/content/content.ts
+++ packages/coil-extension/src/content/content.ts
@@ -1,40 +1,42 @@
-import '@abraham/reflection'
+// import '@abraham/reflection'
+//
+// import { Container } from 'inversify'
+// import { GraphQlClientOptions } from '@coil/client'
+// import { inversifyModule } from '@dier-makr/inversify'
+// import { GlobalModule } from '@dier-makr/annotations'
+//
+// import * as tokens from '../types/tokens'
+// import { API, COIL_DOMAIN } from '../webpackDefines'
+// import { ClientOptions } from '../services/ClientOptions'
+// import { loggingEnabled } from '../util/isLoggingEnabled'
+//
+// import { ContentScript } from './services/ContentScript'
+//
+// function configureContainer(container: Container) {
+//   container.bind(tokens.LoggingEnabled).toConstantValue(loggingEnabled)
+//   container.bind(tokens.ContentRuntime).toConstantValue(API.runtime)
+//   container.bind(tokens.CoilDomain).toConstantValue(COIL_DOMAIN)
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const noop = (..._: unknown[]) => undefined
+//   container
+//     .bind(tokens.Logger)
+//     .toConstantValue(noop)
+//     .whenTargetNamed('CoilClient')
+//   container.bind(GraphQlClientOptions).to(ClientOptions)
+//   container.bind(Storage).toConstantValue(localStorage)
+//   container.bind(Window).toConstantValue(window)
+//   container.bind(Document).toConstantValue(document)
+// }
+//
+// function main() {
+//   const container = new Container({
+//     defaultScope: 'Singleton',
+//     autoBindInjectable: true
+//   })
+//   inversifyModule(GlobalModule)
+//   configureContainer(container)
+//   container.get(ContentScript).init()
+// }
+// main()
 
-import { Container } from 'inversify'
-import { GraphQlClientOptions } from '@coil/client'
-import { inversifyModule } from '@dier-makr/inversify'
-import { GlobalModule } from '@dier-makr/annotations'
-
-import * as tokens from '../types/tokens'
-import { API, COIL_DOMAIN } from '../webpackDefines'
-import { ClientOptions } from '../services/ClientOptions'
-import { loggingEnabled } from '../util/isLoggingEnabled'
-
-import { ContentScript } from './services/ContentScript'
-
-function configureContainer(container: Container) {
-  container.bind(tokens.LoggingEnabled).toConstantValue(loggingEnabled)
-  container.bind(tokens.ContentRuntime).toConstantValue(API.runtime)
-  container.bind(tokens.CoilDomain).toConstantValue(COIL_DOMAIN)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const noop = (..._: unknown[]) => undefined
-  container
-    .bind(tokens.Logger)
-    .toConstantValue(noop)
-    .whenTargetNamed('CoilClient')
-  container.bind(GraphQlClientOptions).to(ClientOptions)
-  container.bind(Storage).toConstantValue(localStorage)
-  container.bind(Window).toConstantValue(window)
-  container.bind(Document).toConstantValue(document)
-}
-
-function main() {
-  const container = new Container({
-    defaultScope: 'Singleton',
-    autoBindInjectable: true
-  })
-  inversifyModule(GlobalModule)
-  configureContainer(container)
-  container.get(ContentScript).init()
-}
-main()
+console.log('content.ts')
