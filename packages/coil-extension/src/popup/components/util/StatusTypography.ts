bb28ff2618222354d47173c45553625bb3c7e36b
Nicholas Dudfield <ndudfield@gmail.com>
2022-06-17 13:59:33 +0700

    refactor: delete unused popup components (#3003)

--- packages/coil-extension/src/popup/components/util/StatusTypography.ts
+++
@@ -1,6 +0,0 @@
-import { styled, Typography } from '@material-ui/core'
-
-export const StatusTypography = styled(Typography)(({ theme }) => ({
-  paddingTop: theme.spacing(1),
-  paddingBottom: theme.spacing(1)
-}))
