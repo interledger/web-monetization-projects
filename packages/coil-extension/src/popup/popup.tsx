38e9a629b5a207d259533f854a9c5ada0d5a0649
Nicholas Dudfield <ndudfield@gmail.com>
2022-06-28 16:46:57 +0700

    fix(coil-extension): revert workaround for early versions of safari wext

--- packages/coil-extension/src/popup/popup.tsx
+++ packages/coil-extension/src/popup/popup.tsx
@@ -13,8 +13,6 @@ import { PopupHost } from './types'
 import { StorageEventPartial } from './context/storeContext'
 import { IndexWithRoot } from './Index'
 
-const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-
 export function run() {
   const rootEl = document.getElementById('root')
 
@@ -31,36 +29,12 @@ export function run() {
         sendMessage: API.runtime.sendMessage.bind(API.runtime)
       }
     }
-    // TODO: this is fixed in Safari 15
     window.addEventListener('storage', e => {
-      if (e.key === '$$popupCommand' && e.newValue) {
-        // Remove the timestamp which will cause a unique string used to trigger
-        // a `storage` event.
-        const command = e.newValue.substring(16)
-        const cmd: ToPopupMessage = JSON.parse(command)
-        if (cmd.command === 'closePopup') {
-          // window.close() itself actually causes a bad state on safari
-          if (isSafari) {
-            // eslint-disable-next-line no-console
-            console.warn(
-              'Should be running window.close(), ' +
-                'but it is buggy on safari, navigator.userAgent=' +
-                navigator.userAgent +
-                '\n' +
-                'see: ' +
-                'https://github.com/coilhq/web-monetization-projects/issues/1077'
-            )
-          } else {
-            window.close()
-          }
-        }
-      } else {
-        const event: StorageEventPartial = {
-          key: e.key,
-          newValue: e.newValue
-        }
-        host.events.emit('storage', event)
+      const event: StorageEventPartial = {
+        key: e.key,
+        newValue: e.newValue
       }
+      host.events.emit('storage', event)
     })
     ReactDOM.render(
       <IndexWithRoot storage={new StorageService(localStorage)} host={host} />,
