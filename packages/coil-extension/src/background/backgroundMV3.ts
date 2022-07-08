c9deed39cfcb678a505ec8f55180bfe349f0a181
Nicholas Dudfield <ndudfield@gmail.com>
2022-07-08 11:11:07 +0700

    chore(deps): yarn patch ilp-protocol-stream/ilp-plugin-btp for MV3 use
    See following pull requests for follow up
    * https://github.com/interledgerjs/ilp-plugin-btp/pull/71
    * https://github.com/interledgerjs/interledgerjs/pull/291

--- packages/coil-extension/src/background/backgroundMV3.ts
+++ packages/coil-extension/src/background/backgroundMV3.ts
@@ -1,5 +1,11 @@
 import { Reloader } from './mv3/reloader'
 
+// These required "yarn patch"ing in order to work
+// The crypto object simply needs to be retrieved from "self" rather
+// than "window".
+import 'ilp-protocol-stream'
+import 'ilp-plugin-btp'
+
 export const dbg = console.log.bind('ServiceWorker')
 dbg('backgroundMV3.ts')
 dbg('typeof self.localStorage', self.localStorage)
