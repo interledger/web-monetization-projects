e20f8691ecdac1044f0a7dddf68bab1c242dc704
Nicholas Dudfield <ndudfield@gmail.com>
2022-07-11 14:05:30 +0700

    feat(coil-extension): wip on polyfill loading

--- packages/coil-extension/webpack.common.ts
+++ packages/coil-extension/webpack.common.ts
@@ -1,21 +1,20 @@
 import { createHash } from 'crypto'
 import * as fs from 'fs'
-import * as path from 'path'
 
 import { makeWebpackConfig } from '@webexts/build-utils'
 import { wmPolyfill } from '@webmonetization/wext/content'
 import merge from 'webpack-merge'
 import * as webpack from 'webpack'
 
-const data = Buffer.from(wmPolyfill, 'utf-8')
-const digest = createHash('sha256').update(data).digest()
+const polyfill = Buffer.from(wmPolyfill, 'utf-8')
+const digest = createHash('sha256').update(polyfill).digest()
 const polyfillHash = `sha256-${digest.toString('base64')}`
 
 // write polyfill
-fs.writeFileSync(`${__dirname}/dist/${polyfillHash}.js`, data)
+// fs.writeFileSync(`${__dirname}/dist/${polyfillHash}.js`, polyfill)
 
 export const config = merge(
-  makeWebpackConfig({ rootDir: __dirname, polyfillHash }),
+  makeWebpackConfig({ rootDir: __dirname, polyfillHash, polyfill: wmPolyfill }),
   {
     plugins: [
       new webpack.DefinePlugin({
