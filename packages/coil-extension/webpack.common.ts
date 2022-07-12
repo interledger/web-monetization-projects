218f0d3958448a56116fd93853a10286b1d7b191
Nicholas Dudfield <ndudfield@gmail.com>
2022-07-12 11:30:45 +0700

    refactor(coil-extension): inject polyfill as script.src

--- packages/coil-extension/webpack.common.ts
+++ packages/coil-extension/webpack.common.ts
@@ -9,15 +9,14 @@ const data = Buffer.from(wmPolyfill, 'utf-8')
 const digest = createHash('sha256').update(data).digest()
 const polyfillHash = `sha256-${digest.toString('base64')}`
 
-export const config = merge(
-  makeWebpackConfig({ rootDir: __dirname, polyfillHash }),
-  {
-    plugins: [
-      new webpack.DefinePlugin({
-        WEBPACK_DEFINE_BTP_ENDPOINT: JSON.stringify(
-          process.env.BTP_ENDPOINT || null
-        )
-      })
-    ]
-  }
-)
+const options = { rootDir: __dirname, polyfillHash, polyfill: wmPolyfill }
+
+export const config = merge(makeWebpackConfig(options), {
+  plugins: [
+    new webpack.DefinePlugin({
+      WEBPACK_DEFINE_BTP_ENDPOINT: JSON.stringify(
+        process.env.BTP_ENDPOINT || null
+      )
+    })
+  ]
+})
