e94964ec9acf38d2bfd574c2097c91eecf69737c
Nicholas Dudfield <ndudfield@gmail.com>
2022-07-14 13:19:14 +0700

    feat(webexts-build-utils): add MV3=eventspage support

--- packages/webexts-build-utils/src/env.ts
+++ packages/webexts-build-utils/src/env.ts
@@ -3,6 +3,9 @@ export const AFTER_DONE_SHELL_CMD = process.env.AFTER_DONE_SHELL_CMD
 
 export const MV3 = Boolean(process.env.MV3 ?? false)
 
+export const MV3_BACKGROUND_TYPE: 'serviceworker' | 'eventspage' =
+  process.env.MV3 === 'eventspage' ? 'eventspage' : 'serviceworker'
+
 export const WEXT_MANIFEST_SUFFIX = process.env.WEXT_MANIFEST_SUFFIX
 export const WEXT_MANIFEST_SUFFIX_NO_DATE =
   process.env.WEXT_MANIFEST_SUFFIX_NO_DATE
