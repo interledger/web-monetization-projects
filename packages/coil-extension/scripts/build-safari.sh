d48cb41dba61d2175b5e4a371e52afc6e9c57741
Nicholas Dudfield <ndudfield@gmail.com>
2022-07-19 16:32:29 +0700

    feat(ci): build safari extension (#3152)
    * feat(ci): build safari extension
    
    * fix(ci): actually add to jobs list
    
    * fix(ci): add checkout steps
    
    * fix(ci): use own cache for macos
    
    * fix(ci): use shell script suffix
    
    * fix(ci): remove missing/useless for safari hot-reload.js

--- packages/coil-extension/scripts/build-safari.sh
+++ packages/coil-extension/scripts/build-safari.sh
@@ -1,3 +1,10 @@
+#!/bin/bash -u
+# We use set -e and bash with -u to bail on first non zero exit code of any
+# processes launched or upon any unbound variable.
+# We use set -x to print commands before running them to help with
+# debugging.
+set -ex
+
 SAFARI_DIR=${SAFARI_DIR:-safari}
 SAFARI_SCHEME=${SAFARI_SCHEME:-Coil (macOS)}
 xcodebuild build -scheme "$SAFARI_SCHEME" -project "$SAFARI_DIR"/Coil/Coil.xcodeproj
