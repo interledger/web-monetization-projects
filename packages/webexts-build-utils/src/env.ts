e6525f4cc97c4b3f6e4a913e3598b8e77e08a10f
Nicholas Dudfield <ndudfield@gmail.com>
2022-09-14 14:50:12 +0700

    feat(coil-extension): add safari as browser type to extension builds

--- packages/webexts-build-utils/src/env.ts
+++ packages/webexts-build-utils/src/env.ts
@@ -1,19 +1,41 @@
 // Possible to override name/version so can publish as different extension
-import { ManifestV2 } from './types/manifest'
+
+function enu<T extends string>(
+  name: string,
+  possibleValues: T[],
+  defaultValue: T
+) {
+  const value = process.env[name]
+  if (typeof value === undefined) {
+    return defaultValue
+  } else if (value && possibleValues.includes(value as T)) {
+    return value as T
+  } else {
+    throw new Error(`${name} must be one of ${possibleValues.join(', ')}`)
+  }
+}
 
 export const AFTER_DONE_SHELL_CMD = process.env.AFTER_DONE_SHELL_CMD
 
 export const MV3 = Boolean(process.env.MV3 ?? false)
 
-export const MV3_BACKGROUND_TYPE: 'serviceworker' | 'eventspage' =
-  process.env.MV3 === 'eventspage' ? 'eventspage' : 'serviceworker'
+export const MV3_BACKGROUND_TYPE = enu(
+  'MV3',
+  ['serviceworker', 'eventspage'],
+  'serviceworker'
+)
 
-export const MV2_BACKGROUND_TYPE: 'backgroundpage' | 'eventspage' =
-  process.env.MV2 === 'eventspage' ? 'eventspage' : 'backgroundpage'
+export const MV2_BACKGROUND_TYPE = enu(
+  'MV2',
+  ['backgroundpage', 'eventspage'],
+  'backgroundpage'
+)
 
-export const WEXT_MANIFEST_INCOGNITO =
-  (process.env.WEXT_MANIFEST_INCOGNITO as ManifestV2['incognito']) ??
-  (MV3 ? 'not_allowed' : 'spanning')
+export const WEXT_MANIFEST_INCOGNITO = enu(
+  'WEXT_MANIFEST_INCOGNITO',
+  ['not_allowed', 'spanning', 'split'],
+  'spanning'
+)
 
 export const WEXT_MANIFEST_SUFFIX = process.env.WEXT_MANIFEST_SUFFIX
 export const WEXT_MANIFEST_SUFFIX_NO_DATE =
@@ -25,8 +47,14 @@ export const WEXT_MANIFEST_BROWSER_SPECIFIC_SETTINGS_GECKO_ID =
 export const WEXT_MANIFEST_KEY = process.env.WEXT_MANIFEST_KEY
 export const WEXT_MANIFEST_PERMISSIONS = process.env.WEXT_MANIFEST_PERMISSIONS
 
-export const API = process.env.API || 'chrome'
-export const BROWSER = process.env.BROWSER || 'chrome'
+export const API = enu('API', ['chrome', 'browser'], 'chrome')
+
+export const BROWSER = enu(
+  'BROWSER',
+  ['chrome', 'safari', 'edge', 'firefox', 'samsung-internet'],
+  'chrome'
+)
+
 export const LIVE_RELOAD =
   Boolean(process.env.LIVE_RELOAD) && !process.env.NO_LIVE_RELOAD
 
