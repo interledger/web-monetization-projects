01f4ec7a63d3a0a6ecc42c39baaa7043cd504afa
Nicholas Dudfield <ndudfield@gmail.com>
2023-02-20 10:40:01 +0700

    refactor: fix chatgpts crappy code

--- packages/coil-monorepo-upkeep/src/utils/loadWorkspaces.ts
+++ packages/coil-monorepo-upkeep/src/utils/loadWorkspaces.ts
@@ -20,56 +20,23 @@ interface WorkspaceGlob {
   exclusions: string[]
 }
 
-const compareFn = (a: LernaListItem, b: LernaListItem) =>
+interface Workspace {
+  location: string
+  packageJson: PackageJson
+}
+
+const nameComparator = (a: LernaListItem, b: LernaListItem) =>
   a.name < b.name ? -1 : 1
 
-async function loadWorkspaceFolders(givenFolder: string) {
-  // Load package.json from given folder
-  const packageJson = await fs.promises.readFile(
-    `${givenFolder}/package.json`,
-    'utf-8'
-  )
-  const { workspaces } = JSON.parse(packageJson) as PackageJson
-
-  let workspaceGlobs: WorkspaceGlob[] = []
-
-  if (workspaces) {
-    workspaceGlobs = workspaces.map((workspace: string) => {
-      if (workspace.startsWith('!')) {
-        throw new Error('Workspace globs starting with ! are not supported.')
-      }
-      return {
-        pattern: `${givenFolder}/${workspace}`,
-        exclusions: []
-      }
-    })
-  } else {
-    // Load pnpm-workspace.yaml from given folder
-    const pnpmWorkspaceYaml = await fs.promises.readFile(
-      `${givenFolder}/pnpm-workspace.yaml`,
-      'utf-8'
-    )
-    const parsedPnpmWorkspaceYaml = YAML.parse(pnpmWorkspaceYaml)
-
-    if (!parsedPnpmWorkspaceYaml.packages) {
-      throw new Error('No packages found in pnpm-workspace.yaml')
-    }
-
-    const exclusions = parsedPnpmWorkspaceYaml.packages
-      .filter((exclusion: string) => exclusion.startsWith('!'))
-      .map((exclusion: string) => `${givenFolder}/${exclusion.substring(1)}`)
-
-    workspaceGlobs = parsedPnpmWorkspaceYaml.packages
-      .filter((pattern: string) => !pattern.startsWith('!'))
-      .map((pattern: string) => {
-        return {
-          pattern: `${givenFolder}/${pattern}`,
-          exclusions
-        }
-      })
+const borrowedFields = (packageJson: PackageJson) => {
+  return {
+    name: packageJson.name,
+    version: packageJson.version,
+    private: packageJson.private ?? false
   }
+}
 
-  // Execute the specified list of globs relative to the initial givenFolder
+async function executeGlobs(workspaceGlobs: WorkspaceGlob[]) {
   const workspaceFolders: string[] = (
     await Promise.all(
       workspaceGlobs.map(async (workspaceGlob: WorkspaceGlob) => {
@@ -92,69 +59,126 @@ async function loadWorkspaceFolders(givenFolder: string) {
   return workspaceFolders
 }
 
-export async function loadWorkspacePackages(
-  givenFolder: string
-): Promise<LernaListItem[]> {
-  const workspaceFolders = await loadWorkspaceFolders(givenFolder)
+async function getPNPMWorkspaceGlobs(givenFolder: string) {
+  // Load pnpm-workspace.yaml from given folder
+  const pnpmWorkspaceYaml = await fs.promises.readFile(
+    `${givenFolder}/pnpm-workspace.yaml`,
+    'utf-8'
+  )
+  const parsedPnpmWorkspaceYaml = YAML.parse(pnpmWorkspaceYaml)
 
-  // Load the package.json file for each workspace folder
-  const workspacesWithPackageJsons = await Promise.all(
+  if (!parsedPnpmWorkspaceYaml.packages) {
+    throw new Error('No packages found in pnpm-workspace.yaml')
+  }
+
+  const exclusions = parsedPnpmWorkspaceYaml.packages
+    .filter((exclusion: string) => exclusion.startsWith('!'))
+    .map((exclusion: string) => `${givenFolder}/${exclusion.substring(1)}`)
+
+  const pnpmGlobs = parsedPnpmWorkspaceYaml.packages
+    .filter((pattern: string) => !pattern.startsWith('!'))
+    .map((pattern: string) => {
+      return {
+        pattern: `${givenFolder}/${pattern}`,
+        exclusions
+      }
+    })
+  return pnpmGlobs
+}
+
+async function getWorkspaceFolders(givenFolder: string) {
+  // Load package.json from given folder
+  const packageJson = await fs.promises.readFile(
+    `${givenFolder}/package.json`,
+    'utf-8'
+  )
+  const { workspaces } = JSON.parse(packageJson) as PackageJson
+  let workspaceGlobs: WorkspaceGlob[]
+
+  if (workspaces) {
+    workspaceGlobs = workspaces.map((workspace: string) => {
+      if (workspace.startsWith('!')) {
+        throw new Error('Workspace globs starting with ! are not supported.')
+      }
+      return {
+        pattern: `${givenFolder}/${workspace}`,
+        exclusions: []
+      }
+    })
+  } else {
+    // TODO, check if pnpm file exists
+    workspaceGlobs = await getPNPMWorkspaceGlobs(givenFolder)
+  }
+
+  // Execute the specified list of globs relative to the initial givenFolder
+  return await executeGlobs(workspaceGlobs)
+}
+
+function formatListItems(
+  locationAndPackageJSONs: Awaited<{
+    packageJson: PackageJson
+    location: string
+  }>[]
+) {
+  return locationAndPackageJSONs.map(({ location, packageJson }) => {
+    const dependencies: LernaListItem[] = []
+    for (const depName in {
+      ...packageJson.dependencies,
+      ...packageJson.devDependencies
+    }) {
+      const dep = locationAndPackageJSONs.find(
+        workspace => workspace.packageJson.name === depName
+      )
+
+      if (dep) {
+        dependencies.push({
+          ...borrowedFields(dep.packageJson),
+          location: dep.location,
+          dependencies: []
+        })
+      }
+    }
+
+    // Sort the dependencies by package name
+    dependencies.sort(nameComparator)
+
+    return {
+      ...borrowedFields(packageJson),
+      location,
+      dependencies
+    }
+  })
+}
+
+async function loadWorkspaces(
+  workspaceFolders: string[]
+): Promise<Workspace[]> {
+  return await Promise.all(
     workspaceFolders.map(async (folderPath: string) => {
       const packageJson = await fs.promises.readFile(
         `${folderPath}/package.json`,
         'utf-8'
       )
       const parsedPackageJson = JSON.parse(packageJson) as PackageJson
-      return { path: folderPath, packageJson: parsedPackageJson }
+      return { location: folderPath, packageJson: parsedPackageJson }
     })
   )
+}
+
+export async function loadWorkspacePackages(
+  givenFolder: string
+): Promise<LernaListItem[]> {
+  const workspaceFolders = await getWorkspaceFolders(givenFolder)
+
+  // Load the package.json file for each workspace folder
+  const locationAndPackageJSONs = await loadWorkspaces(workspaceFolders)
 
   // Create LernaListItem objects for each workspace and its dependencies
-  const lernaListItems: LernaListItem[] = workspacesWithPackageJsons.map(
-    ({ path, packageJson }) => {
-      const dependencies: LernaListItem[] = []
-
-      const {
-        name: pkgName,
-        version: pkgVersion,
-        private: isPkgPrivate,
-        dependencies: pkgDependencies,
-        devDependencies: pkgDevDependencies
-      } = packageJson
-
-      for (const depName in {
-        ...pkgDependencies,
-        ...pkgDevDependencies
-      }) {
-        const dep = workspacesWithPackageJsons.find(
-          workspace => workspace.packageJson.name === depName
-        )
-
-        if (dep) {
-          dependencies.push({
-            name: dep.packageJson.name,
-            version: dep.packageJson.version,
-            private: dep.packageJson.private ?? false,
-            location: dep.path,
-            dependencies: []
-          })
-        }
-      }
-
-      // Sort the dependencies by package name
-      dependencies.sort(compareFn)
-
-      return {
-        name: pkgName,
-        version: pkgVersion,
-        private: isPkgPrivate ?? false,
-        location: path,
-        dependencies
-      }
-    }
+  const lernaListItems: LernaListItem[] = formatListItems(
+    locationAndPackageJSONs
   )
 
   // Sort the list of LernaListItem objects by package name
-  lernaListItems.sort(compareFn)
+  lernaListItems.sort(nameComparator)
   return lernaListItems
 }
