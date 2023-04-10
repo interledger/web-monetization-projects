6ca1f642ebdd9a1ad0de8d2822a51d70f86f10a1
Nicholas Dudfield <ndudfield@gmail.com>
2023-04-10 12:47:54 +0700

    fix: try use jest.config.cjs

--- packages/coil-monorepo-upkeep/templates/static-shared-duplicated/jest.config.cjs
+++ packages/coil-monorepo-upkeep/templates/static-shared-duplicated/jest.config.cjs
@@ -2,16 +2,20 @@
 const path = require('path')
 
 const displayName = path.basename(__dirname)
-const rootConfig = { ...require('../../jest.config') }
+const rootConfig = { ...require('../../jest.config.cjs') }
 delete rootConfig.projects
 delete rootConfig.rootDir
+
+const coverage = {
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: 'results/coverage',
+}
 
 module.exports = {
   ...rootConfig,
   displayName,
-  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  coverageDirectory: 'results/coverage',
+  ...(process.env.PROJECT_JEST === '1' ? coverage : undefined),
   testMatch: [
     '<rootDir>/test/jest/**/*.test.ts?(x)',
     '<rootDir>/src/**/*.test.ts?(x)'
