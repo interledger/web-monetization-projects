b57ce31b3a03645e5360cd62a7e227303d21f9ab
Nicholas Dudfield <ndudfield@gmail.com>
2023-04-21 12:39:00 +0700

    feat: move to pnpm

--- packages/coil-privacypass-sjcl/jest-e2e.config.cjs
+++ packages/coil-privacypass-sjcl/jest-e2e.config.cjs
@@ -1,5 +1,5 @@
 // FROM UPKEEP TEMPLATE
 module.exports = {
-  ...require('./jest.config.cjs'),
+  ...require(`${__dirname}/jest.config.cjs`),
   testMatch: ['<rootDir>/test/jest-e2e/**/*.test.[jt]s?(x)']
 }
