6143f1bd16f5b7c889860596aa25eec082b93f0c
Nicholas Dudfield <ndudfield@gmail.com>
2023-05-01 14:46:28 +0700

    feat: add typing

--- packages/niq-privacypass-noble/src/crypto/shakePrng.ts
+++ packages/niq-privacypass-noble/src/crypto/shakePrng.ts
@@ -1,6 +1,8 @@
 import { Keccak, shake256 } from '@noble/hashes/sha3'
 
-export function shakePrng(seed: Uint8Array) {
+import { PrngFn } from './types'
+
+export function shakePrng(seed: Uint8Array): PrngFn {
   const prng = shake256.create({}) as Keccak
   prng.update(seed)
   return prng.xof.bind(prng)
