631ec65ab4168aba82d7fab3c494157a8008e460
Nicholas Dudfield <ndudfield@gmail.com>
2023-05-02 10:38:18 +0700

    refactor: use Point type for hashAndInc

--- packages/niq-privacypass-noble/src/crypto/hashAndInc.ts
+++ packages/niq-privacypass-noble/src/crypto/hashAndInc.ts
@@ -1,38 +1,36 @@
 import { CHash, concatBytes } from '@noble/hashes/utils'
-import { CurveFn, ProjPointType } from '@noble/curves/abstract/weierstrass'
+import { CurveFn } from '@noble/curves/abstract/weierstrass'
 import { p256 } from '@noble/curves/p256'
 import { sha256 } from '@noble/hashes/sha256'
 
-import { Hashable } from './types'
+import { Hashable, Point } from './types'
 
 const LABEL = '1.2.840.10045.3.1.7 point generation seed'
 const YBYTE = new Uint8Array([0x02])
 
 export function hashAndInc(
   seed: Uint8Array,
-  HashClass: CHash = sha256,
+  hash: CHash = sha256,
   // We can't use CryptoContext or Config here
   curve: CurveFn = p256,
   label: Hashable = LABEL
-): ProjPointType<bigint> {
-  let h = HashClass.create()
+): Point {
+  let h = hash.create()
   h.update(label)
 
-  let i = 0
-  for (i = 0; i < 20; i++) {
-    const ctr = new Uint8Array(4)
-    ctr[0] = (i >>> 0) & 0xff
+  for (let i = 0; i < 20; i++) {
+    const ctr = new Uint8Array([i, 0, 0, 0])
 
     h.update(seed)
     h.update(ctr)
 
-    const digestBytes = h.digest()
-    const potentialPoint = concatBytes(YBYTE, digestBytes)
+    const xBytes = h.digest()
+    const potentialPoint = concatBytes(YBYTE, xBytes)
     try {
       return curve.ProjectivePoint.fromHex(potentialPoint)
-    } catch (e) {
-      seed = digestBytes
-      h = HashClass.create()
+    } catch (e: unknown) {
+      seed = xBytes
+      h = hash.create()
     }
   }
 
