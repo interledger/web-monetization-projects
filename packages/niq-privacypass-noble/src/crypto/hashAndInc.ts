d310b18589da2d10c97918183ea3e13cc95389f7
Nicholas Dudfield <ndudfield@gmail.com>
2023-05-02 10:26:07 +0700

    refactor: simplify hashAndInc label

--- packages/niq-privacypass-noble/src/crypto/hashAndInc.ts
+++ packages/niq-privacypass-noble/src/crypto/hashAndInc.ts
@@ -3,14 +3,9 @@ import { CurveFn, ProjPointType } from '@noble/curves/abstract/weierstrass'
 import { p256 } from '@noble/curves/p256'
 import { sha256 } from '@noble/hashes/sha256'
 
-// 1.2.840.10045.3.1.7 point generation seed
-const INC_H2C_LABEL = new Uint8Array([
-  0x31, 0x2e, 0x32, 0x2e, 0x38, 0x34, 0x30, 0x2e, 0x31, 0x30, 0x30, 0x34, 0x35,
-  0x2e, 0x33, 0x2e, 0x31, 0x2e, 0x37, 0x20, 0x70, 0x6f, 0x69, 0x6e, 0x74, 0x20,
-  0x67, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x73, 0x65,
-  0x65, 0x64
-])
+import { Hashable } from './types'
 
+const LABEL = '1.2.840.10045.3.1.7 point generation seed'
 const YBYTE = new Uint8Array([0x02])
 
 export function hashAndInc(
@@ -18,7 +13,7 @@ export function hashAndInc(
   HashClass: CHash = sha256,
   // We can't use CryptoContext or Config here
   curve: CurveFn = p256,
-  label: Uint8Array = INC_H2C_LABEL
+  label: Hashable = LABEL
 ): ProjPointType<bigint> {
   let h = HashClass.create()
   h.update(label)
